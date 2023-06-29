8cec178bb8c37c189d8fbe445ba1d6bd577bb8a9
Yasnovsky <andrey5.1997@mail.ru>
2023-06-29 17:11:49 +0500

    feat(compensations): #865cmg3r2: add render empty message

--- src/features/compensations/components/CompensationsTable.tsx
+++ src/features/compensations/components/CompensationsTable.tsx
@@ -1,6 +1,8 @@
 import moment from 'moment';
 import { formatMoney } from '../../../common/utils/formatMoney';
 
+const NO_DATA = 'Not';
+
 type CompensationsType = {
   list: { date: string; comment: string; amount: number; isUnpaid: boolean }[],
   totalUnpaidAmount: number;
@@ -13,24 +15,28 @@ function CompensationsTable({
 }) {
   return (
     <div data-cy="compensations-table">
-      {compensations.list.map(({
-        date, amount, comment,
-      }) => (
-        <div
-          data-cy="compensations-table-row"
-          key={date}
-        >
-          <span>{moment(date).format('MMMM YYYY')}</span>
-          <span>{moment(date).format('DD.MM.YYYY')}</span>
-          <span>{comment}</span>
-          <span>{formatMoney(amount)}</span>
-        </div>
-      ))}
+      {compensations.list.length !== 0 ? (
+        <>
+          {compensations.list.map(({
+            date, amount, comment,
+          }) => (
+            <div data-cy="compensations-table-row" key={date}>
+              <span>{moment(date).format('MMMM YYYY')}</span>
+              <span>{moment(date).format('DD.MM.YYYY')}</span>
+              <span>{comment}</span>
+              <span>{formatMoney(amount)}</span>
+            </div>
+          ))}
 
-      <div data-cy="compensations-table-row-total">
-        <span>Unpaid</span>
-        <span>{formatMoney(compensations.totalUnpaidAmount)}</span>
-      </div>
+          <div data-cy="compensations-table-row-total">
+            <span>Unpaid</span>
+            <span>{formatMoney(compensations.totalUnpaidAmount)}</span>
+          </div>
+        </>
+      ) : (
+        <div data-cy="compensations-table-no-data">{NO_DATA}</div>
+      ) }
+
     </div>
   );
 }
