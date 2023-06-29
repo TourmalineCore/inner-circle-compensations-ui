ca82839aa09ab2fdb8e2f8c5ef506ea74fbb35de
Yasnovsky <andrey5.1997@mail.ru>
2023-06-29 16:48:08 +0500

    test(compensations): #865cmg3r2: check render row in CompensationsTable where have data

--- src/features/compensations/components/CompensationsTable.cy.tsx
+++ src/features/compensations/components/CompensationsTable.cy.tsx
@@ -2,6 +2,10 @@ import '../../../../cypress/support/commands';
 
 import CompensationsTable from './CompensationsTable';
 
+type CompensationsType = {
+  list: { date: string; comment: string; amount: number; isUnpaid: boolean }[],
+};
+
 describe('CompensationsTable', () => {
   it('SHOULD render roles table WHEN visit roles page', () => {
     mountComponent();
@@ -9,10 +13,32 @@ describe('CompensationsTable', () => {
     cy.getByData('compensations-table')
       .should('exist');
   });
+
+  it('SHOULD render compensations table row WHEN have data', () => {
+    mountComponent({
+      compensations: {
+        list: [
+          {
+            date: '2023-06-08T11:42:04.467165Z',
+            comment: 'I bought milk',
+            amount: 760,
+            isUnpaid: false,
+          },
+        ],
+      },
+    });
+
+    cy.getByData('compensations-table-row')
+      .should('exist');
+  });
 });
 
-function mountComponent() {
+function mountComponent({
+  compensations,
+}: {
+  compensations: CompensationsType;
+}) {
   cy.mount(
-    <CompensationsTable />,
+    <CompensationsTable compensations={compensations} />,
   );
 }
