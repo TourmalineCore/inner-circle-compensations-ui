b579596e64eddba725293d0272205bc258d7f5cc
Yasnovsky <andrey5.1997@mail.ru>
2023-06-29 16:57:33 +0500

    feat(compensations): #865cmg3r2: add render row with elements

--- src/features/compensations/components/CompensationsTable.tsx
+++ src/features/compensations/components/CompensationsTable.tsx
@@ -1,7 +1,31 @@
-function CompensationsTable() {
+import moment from 'moment';
+import { formatMoney } from '../../../common/utils/formatMoney';
+
+type CompensationsType = {
+  list: { date: string; comment: string; amount: number; isUnpaid: boolean }[],
+};
+
+function CompensationsTable({
+  compensations,
+}: {
+  compensations: CompensationsType,
+}) {
   return (
     <div data-cy="compensations-table">
-      CompensationsTable
+      {compensations.list.map(({
+        date, amount, comment,
+      }) => (
+        <div
+          data-cy="compensations-table-row"
+          key={date}
+        >
+          <span>{moment(date).format('MMMM YYYY')}</span>
+          <span>{moment(date).format('DD.MM.YYYY')}</span>
+          <span>{comment}</span>
+          <span>{formatMoney(amount)}</span>
+        </div>
+      ))}
+
     </div>
   );
 }
