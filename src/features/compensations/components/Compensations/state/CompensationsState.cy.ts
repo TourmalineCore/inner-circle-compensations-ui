d351f286edf682464600f835966378eb54c9f399
Yasnovsky <andrey5.1997@mail.ru>
2023-06-30 10:21:58 +0500

    test(compensations): #865cmg3r2: check add initialize compensations

---
+++ src/features/compensations/components/Compensations/state/CompensationsState.cy.ts
@@ -0,0 +1,26 @@
+import '../../../../../../cypress/support/commands';
+
+import CompensationsState from './CompensationsState';
+
+describe('RolesPageState', () => {
+  it('SHOULD return all compensations WHEN initialized', () => {
+    const compensationsState = new CompensationsState();
+
+    compensationsState.initialize({
+      loadedCompensations: {
+        list: [
+          {
+            date: '2023-06-08T11:42:04.467165Z',
+            comment: 'I bought milk',
+            amount: 760,
+            isUnpaid: false,
+          },
+        ],
+        totalUnpaidAmount: 760,
+      },
+    });
+
+    expect(compensationsState.allCompensations.list).to.has.lengthOf(1);
+    expect(compensationsState.allCompensations.totalUnpaidAmount).eq(760);
+  });
+});
