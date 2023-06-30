6b9fcde91b8e2b61249a0bea9f7aa0a55055479a
Yasnovsky <andrey5.1997@mail.ru>
2023-06-30 13:59:24 +0500

    refactor(compensations): #865cmg3r2: add filtering for table row

--- src/features/compensations/components/Compensations/state/CompensationsState.ts
+++ src/features/compensations/components/Compensations/state/CompensationsState.ts
@@ -18,7 +18,10 @@ class CompensationsState {
   }
 
   get allCompensations() {
-    return this._compensations;
+    return {
+      list: this._compensations.list.filter((item) => getFiltering(item, this._filterTerm)),
+      totalUnpaidAmount: this._compensations.totalUnpaidAmount,
+    };
   }
 
   get filterTerm() {
@@ -36,6 +39,22 @@ class CompensationsState {
   updateFilterTerm(newFilterTerm: string) {
     this._filterTerm = newFilterTerm;
   }
+}
+
+export function getFiltering(
+  employee: {
+    date: string;
+    comment: string;
+    amount: number;
+    isUnpaid: boolean
+  },
+  filterTerm: string,
+) {
+  if (filterTerm === 'unpaid') {
+    return !employee.isUnpaid;
+  }
+
+  return employee;
 }
 
 export default CompensationsState;
