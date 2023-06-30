998b4fb0180d436dcc8fa9a870fdc3e2bb7a2d12
Yasnovsky <andrey5.1997@mail.ru>
2023-06-30 10:51:38 +0500

    feat(compensations): #865cmg3r2: add render for filter elements

--- src/features/compensations/components/Compensations/components/CompensationsFilter/CompensationsFilter.tsx
+++ src/features/compensations/components/Compensations/components/CompensationsFilter/CompensationsFilter.tsx
@@ -1,6 +1,30 @@
+import { Button } from '@tourmalinecore/react-tc-ui-kit';
+
+const filterElements = [
+  {
+    id: 'all',
+    name: 'All',
+  },
+  {
+    id: 'unpaid',
+    name: 'Unpaid',
+  },
+];
+
 function CompensationsFilter() {
   return (
-    <div data-cy="compensations-filter-inner" />
+    <div data-cy="compensations-filter-inner">
+      {filterElements.map((item) => (
+        <Button
+          type="button"
+          data-cy="compensations-filter"
+          key={item.id}
+          id={item.id}
+        >
+          {item.name}
+        </Button>
+      ))}
+    </div>
   );
 }
 
