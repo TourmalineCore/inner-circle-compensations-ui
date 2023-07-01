5af508359c4563b56de78f2d982acb9fe6d8c92b
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 15:00:59 +0500

    feat(create compensations): #85ztdqh7r: add component ListTypesCompensations

---
+++ src/features/compensations/components/CreateCompensations/components/ListTypesCompensations/ListTypesCompensations.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListTypesCompensations() {
+  return (
+    <ul data-cy="list-types-compensations">
+      <li />
+    </ul>
+  );
+}
+
+export default ListTypesCompensations;
