be27d44d3d536bf0ff70ee9ba59592f6bbff748c
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 18:24:38 +0500

    feat(create compensations): #85ztdqh7r: add component DatePicker and setting this component

--- src/features/compensations/components/CreateCompensations/components/DateCompensations/DatePickerCompensations.tsx
+++ src/features/compensations/components/CreateCompensations/components/DateCompensations/DatePickerCompensations.tsx
@@ -1,10 +1,38 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
+import { HTMLProps, forwardRef, useContext } from 'react';
+import DatePicker from 'react-datepicker';
+import { observer } from 'mobx-react-lite';
+import CreateCompensationsStateContext from '../../state/CreateCompensationsStateContext';
+
+const DatePickerCompensationsCustomElement = forwardRef<HTMLButtonElement, HTMLProps<HTMLButtonElement>>(({ value, onClick }, ref) => (
+  <button
+    type="button"
+    onClick={onClick}
+    ref={ref}
+    data-cy="date-picker-compensations-select"
+  >
+    <span data-cy="date-picker-compensations-result">{value}</span>
+    <span>&or;</span>
+  </button>
+));
+
 function DatePickerCompensations() {
+  const createCompensationsState = useContext(CreateCompensationsStateContext);
+
   return (
     <div
       data-cy="date-picker-compensations"
-    />
+    >
+      <span>Month:</span>
+      <DatePicker
+        selected={createCompensationsState.dateCompensation}
+        onChange={(date: Date) => createCompensationsState.updateDate(date)}
+        showMonthYearPicker
+        dateFormat="MMMM yyyy"
+        customInput={<DatePickerCompensationsCustomElement />}
+      />
+    </div>
   );
 }
 
-export default DatePickerCompensations;
+export default observer(DatePickerCompensations);
