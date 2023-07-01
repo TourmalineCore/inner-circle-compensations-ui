20e07ba40ee743abfb2b724bea7cd6389ef44b3c
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 18:25:23 +0500

    styles(create compensations): #85ztdqh7r: add style file and styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import 'react-app-polyfill/stable';
 // above code required for ie11 support
 
 import 'react-loading-skeleton/dist/skeleton.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import '@tourmalinecore/react-tc-ui-kit/es/index.css';
 import '@tourmalinecore/react-tc-modal/es/index.css';
 import '@tourmalinecore/react-table-responsive/es/index.css';
