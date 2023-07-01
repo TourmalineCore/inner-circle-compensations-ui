f1d267351f541e8637b1a7ba9d3ff93f82381b22
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 18:21:42 +0500

    test(create compensations): #85ztdqh7r: check render correct date next year

--- src/features/compensations/components/CreateCompensations/components/DateCompensations/DatePickerCompensations.cy.tsx
+++ src/features/compensations/components/CreateCompensations/components/DateCompensations/DatePickerCompensations.cy.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import '../../../../../../../cypress/support/commands';
+
+import CreateCompensationsState from '../../state/CreateCompensationsState';
+import CreateCompensationsStateContext from '../../state/CreateCompensationsStateContext';
 
 import DatePickerCompensations from './DatePickerCompensations';
 
@@ -9,10 +13,32 @@ describe('ListTypesCompensations', () => {
     cy.getByData('date-picker-compensations')
       .should('exist');
   });
+
+  it('SHOULD render correct date WHEN select next year', () => {
+    mountComponent();
+
+    cy.getByData('date-picker-compensations-select')
+      .click();
+
+    cy.get('[aria-label="Next Year"]')
+      .click();
+
+    cy.contains('Jan')
+      .click();
+
+    cy.getByData('date-picker-compensations-result')
+      .should('have.text', 'January 2024');
+  });
 });
 
 function mountComponent() {
+  const createCompensationsState = new CreateCompensationsState();
+
+  createCompensationsState.updateDate(new Date('2023-08-1'));
+
   cy.mount(
-    <DatePickerCompensations />,
+    <CreateCompensationsStateContext.Provider value={createCompensationsState}>
+      <DatePickerCompensations />
+    </CreateCompensationsStateContext.Provider>,
   );
 }
