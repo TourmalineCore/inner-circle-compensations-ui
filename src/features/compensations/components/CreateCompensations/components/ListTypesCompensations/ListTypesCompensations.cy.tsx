38ac2341a01a53926e72b2a1bab29ed7ed842bc3
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 15:19:37 +0500

    test(create compensations): #85ztdqh7r: check render element types

--- src/features/compensations/components/CreateCompensations/components/ListTypesCompensations/ListTypesCompensations.cy.tsx
+++ src/features/compensations/components/CreateCompensations/components/ListTypesCompensations/ListTypesCompensations.cy.tsx
@@ -1,6 +1,57 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import '../../../../../../../cypress/support/commands';
 
+import CreateCompensationsState from '../../state/CreateCompensationsState';
+import CreateCompensationsStateContext from '../../state/CreateCompensationsStateContext';
+
 import ListTypesCompensations from './ListTypesCompensations';
+
+const INITIAL_TYPES = [
+  {
+    label: 'English',
+    value: 'english',
+  },
+  {
+    label: 'Milk',
+    value: 'milk',
+  },
+  {
+    label: 'German',
+    value: 'german',
+  },
+  {
+    label: 'Swimming',
+    value: 'swimming',
+  },
+  {
+    label: 'Water',
+    value: 'water',
+  },
+  {
+    label: 'Coworking',
+    value: 'coworking',
+  },
+  {
+    label: 'Massage',
+    value: 'massage',
+  },
+  {
+    label: 'Products',
+    value: 'products',
+  },
+  {
+    label: 'Consumables',
+    value: 'consumables',
+  },
+  {
+    label: 'Periphery',
+    value: 'periphery',
+  },
+  {
+    label: 'Business trip',
+    value: 'businessTrip',
+  },
+];
 
 describe('ListTypesCompensations', () => {
   it('SHOULD render list types compensations WHEN visit compensations page', () => {
@@ -9,10 +60,26 @@ describe('ListTypesCompensations', () => {
     cy.getByData('list-types-compensations')
       .should('exist');
   });
+
+  it('SHOULD render list type element compensations WHEN have types data', () => {
+    mountComponent();
+
+    cy.getByData('list-types-compensations')
+      .children()
+      .should('have.length', 11);
+  });
 });
 
 function mountComponent() {
+  const createCompensationsState = new CreateCompensationsState();
+
+  createCompensationsState.initializeTypes({
+    loadedTypes: INITIAL_TYPES,
+  });
+
   cy.mount(
-    <ListTypesCompensations />,
+    <CreateCompensationsStateContext.Provider value={createCompensationsState}>
+      <ListTypesCompensations />
+    </CreateCompensationsStateContext.Provider>,
   );
 }
