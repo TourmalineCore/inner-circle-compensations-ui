a2a4e26989e6784a2bb30b94ca7c48afc53467b2
Yasnovsky <andrey5.1997@mail.ru>
2023-07-01 18:13:16 +0500

    feat(create compensations): #85ztdqh7r: create component DatePickerCompensations

---
+++ src/features/compensations/components/CreateCompensations/components/DateCompensations/DatePickerCompensations.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+function DatePickerCompensations() {
+  return (
+    <div
+      data-cy="date-picker-compensations"
+    />
+  );
+}
+
+export default DatePickerCompensations;
