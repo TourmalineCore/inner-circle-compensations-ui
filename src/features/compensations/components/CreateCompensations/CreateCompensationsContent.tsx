213c027a76b5f7dbaf9dc40e54fc1e4f4d66dd16
Yasnovsky <andrey5.1997@mail.ru>
2023-07-27 23:57:39 +0500

    feat(create compensations): #85ztdqh7r: create components and add function for request types

---
+++ src/features/compensations/components/CreateCompensations/CreateCompensationsContent.tsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import CreateCompensationsStateContext from './state/CreateCompensationsStateContext';
+import TableCreateCompensations from './components/TableCreateCompensations/TableCreateCompensations';
+import ListTypesCompensations from './components/ListTypesCompensations/ListTypesCompensations';
+import DatePickerCompensations from './components/DateCompensations/DatePickerCompensations';
+import { api } from '../../../../common/api';
+import { LINK_TO_COMPENSATIONS_SERVICE } from '../../../../common/config/config';
+
+function CreateCompensationsContent() {
+  const createCompensationState = useContext(CreateCompensationsStateContext);
+
+  useEffect(() => {
+    loadCompensationTypes();
+  }, []);
+
+  return (
+    <div>
+      <h2>New compensation</h2>
+      <ListTypesCompensations />
+    </div>
+  );
+
+  async function loadCompensationTypes() {
+    try {
+      const { data } = await api.get(`${LINK_TO_COMPENSATIONS_SERVICE}types`);
+
+      createCompensationState.initializeTypes({ loadedTypes: data });
+    } catch {
+      console.log('error');
+    }
+  }
+}
+
+export default observer(CreateCompensationsContent);
