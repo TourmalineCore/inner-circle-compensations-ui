29b5756faea02abb78134ca69d1d0da56bcd00f5
Yasnovsky <andrey5.1997@mail.ru>
2023-07-27 23:56:17 +0500

    test(create compensations): #85ztdqh7r: check render types after response backend

---
+++ src/features/compensations/components/CreateCompensations/CreateCompensations.cy.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import '../../../../../cypress/support/commands';
+import { API_ROOT, LINK_TO_COMPENSATIONS_SERVICE } from '../../../../common/config/config';
+import CreateCompensations from './CreateCompensations';
+
+const INITIAL_TYPES = [
+  {
+    label: 'English',
+    value: 'english',
+  },
+  {
+    label: 'Milk',
+    value: 'milk',
+  },
+  {
+    label: 'German',
+    value: 'german',
+  },
+  {
+    label: 'Swimming',
+    value: 'swimming',
+  },
+  {
+    label: 'Water',
+    value: 'water',
+  },
+  {
+    label: 'Coworking',
+    value: 'coworking',
+  },
+  {
+    label: 'Massage',
+    value: 'massage',
+  },
+  {
+    label: 'Products',
+    value: 'products',
+  },
+  {
+    label: 'Consumables',
+    value: 'consumables',
+  },
+  {
+    label: 'Periphery',
+    value: 'periphery',
+  },
+  {
+    label: 'Business trip',
+    value: 'businessTrip',
+  },
+];
+
+describe('TableCreateCompensations', () => {
+  it('SHOULD render compensation types WHEN visit page', () => {
+    cy.intercept(
+      'GET',
+      `${API_ROOT}${LINK_TO_COMPENSATIONS_SERVICE}types`,
+      INITIAL_TYPES,
+    );
+
+    mountComponent();
+
+    cy.getByData('list-types-compensations')
+      .children()
+      .should('have.length', 11);
+  });
+});
+
+function mountComponent() {
+  cy.mount(
+    <CreateCompensations />,
+  );
+}
