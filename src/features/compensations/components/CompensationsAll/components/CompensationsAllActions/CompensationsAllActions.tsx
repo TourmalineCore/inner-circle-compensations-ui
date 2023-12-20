c8c8978cac3092f0789c1e2fe27f645be6cce284
akovylyaeva <akovylyaeva@tourmalinecore.com>
2023-12-20 12:28:17 +0500

    chore: hide not active actions

--- src/features/compensations/components/CompensationsAll/components/CompensationsAllActions/CompensationsAllActions.tsx
+++ src/features/compensations/components/CompensationsAll/components/CompensationsAllActions/CompensationsAllActions.tsx
@@ -1,16 +1,18 @@
 import { observer } from 'mobx-react-lite';
 import CompensationsAllFilter from './components/CompensationsAllFilter/CompensationsAllFilter';
 import DateCompensationsAll from './components/DateCompensationsAll/DatePickerCompensationsAll';
-import MarkAsPaidButton from './components/MarkAsPaidButton/MarkAsPaidButton';
-import SearchBarAll from './components/SearchBarAll/SearchBarAll';
+// import MarkAsPaidButton from './components/MarkAsPaidButton/MarkAsPaidButton';
+// import SearchBarAll from './components/SearchBarAll/SearchBarAll';
 
 function CompensationsAllActions() {
   return (
     <div className="compensation-actions" data-cy="compensation-actions">
-      <SearchBarAll />
+      {/* /<SearchBarAll /> */}
+      <div className="compensation-actions--div--left" />
       <DateCompensationsAll />
       <CompensationsAllFilter />
-      <MarkAsPaidButton />
+      {/* <MarkAsPaidButton /> */}
+      <div className="compensation-actions--div--right" />
     </div>
   );
 }
