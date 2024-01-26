713aa3fb9cfe878c242037251dc03d57d53e5436
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-01-26 12:23:16 +0500

    chore: update state tests

--- src/features/compensations/components/Compensations/state/CompensationsState.cy.ts
+++ src/features/compensations/components/Compensations/state/CompensationsState.cy.ts
@@ -1,10 +1,14 @@
 import '../../../../../../cypress/support/commands';
 import { CompensationsState } from './CompensationsState';
 
-describe('CompensationsState', () => {
-  it('SHOULD return all compensations WHEN initialized', () => {
-    const compensationsState = new CompensationsState();
+const compensationsState = new CompensationsState();
 
+describe('CompensationsState', () => {
+  it(`
+  GIVEN compensations personal page 
+  WHEN initialized
+  THEN return all personal compensations
+  `, () => {
     compensationsState.initialize({
       loadedCompensations: {
         list: [
@@ -25,11 +29,15 @@ describe('CompensationsState', () => {
     expect(compensationsState.allCompensations.totalUnpaidAmount).eq(760);
   });
 
-  it('SHOULD get value filter WHEN called update', () => {
-    const compensationsState = new CompensationsState();
-
+  it(`
+  GIVEN compensations personal page 
+  WHEN called update filter
+  THEN get value filter
+  `, () => {
     compensationsState.updateFilterTerm('all');
-
     expect(compensationsState.filterTerm).eq('all');
+
+    compensationsState.updateFilterTerm('unpaid');
+    expect(compensationsState.filterTerm).eq('unpaid');
   });
 });
