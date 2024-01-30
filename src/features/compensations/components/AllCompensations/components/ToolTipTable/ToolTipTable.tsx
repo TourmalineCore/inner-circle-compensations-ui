e340945300af3f5cff69e767d8fda1b31daa1968
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-01-30 09:10:55 +0500

    chore: change tooltiptable classnames

--- src/features/compensations/components/AllCompensations/components/ToolTipTable/ToolTipTable.tsx
+++ src/features/compensations/components/AllCompensations/components/ToolTipTable/ToolTipTable.tsx
@@ -6,13 +6,13 @@ export function ToolTipTable({
   compensations: EmployeeAllCompensationsItemType[]
 }) {
   return (
-    <div className="tooltip-item">
-      <table data-cy="tooltip-table" className="tooltip-table">
+    <div className="tooltip-table">
+      <table data-cy="tooltip-table" className="tooltip-table__table">
         <thead>
-          <tr className="tooltip-table__head" data-cy="tooltip-table-head">
-            <th className="tooltip-table__column-type">Type</th>
-            <th className="tooltip-table__column-comment">Comment</th>
-            <th className="tooltip-table__column-amount">Amount</th>
+          <tr className="tooltip-table__table__head" data-cy="tooltip-table-head">
+            <th className="tooltip-table__table__column-type">Type</th>
+            <th className="tooltip-table__table__column-comment">Comment</th>
+            <th className="tooltip-table__table__column-amount">Amount</th>
           </tr>
         </thead>
         <tbody>
@@ -22,26 +22,26 @@ export function ToolTipTable({
             <tr
               data-cy="tooltip-table-item"
               key={id}
-              className="tooltip-table__item"
+              className="tooltip-table__table__item"
             >
 
               <td
                 data-cy="tooltip-table-column-type"
-                className="tooltip-table__column-type"
+                className="tooltip-table__table__column-type"
               >
                 {compensationType}
               </td>
 
               <td
                 data-cy="tooltip-table-column-comment"
-                className="tooltip-table__column-comment"
+                className="tooltip-table__table__column-comment"
               >
                 {comment}
               </td>
 
               <td
                 data-cy="tooltip-table-column-amount"
-                className="tooltip-table__column-amount"
+                className="tooltip-table__table__column-amount"
               >
                 {formatMoney(amount)}
 
