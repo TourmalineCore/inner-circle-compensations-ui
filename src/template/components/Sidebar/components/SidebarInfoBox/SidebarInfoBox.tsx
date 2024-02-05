255d78fbb90982224a34d25acc69edd7e79bc581
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-02-05 13:12:24 +0500

    chore: improve full name in sidebar and test it

--- src/template/components/Sidebar/components/SidebarInfoBox/SidebarInfoBox.tsx
+++ src/template/components/Sidebar/components/SidebarInfoBox/SidebarInfoBox.tsx
@@ -5,13 +5,25 @@ function SidebarInfoBox({
 }: {
   name: string;
 }) {
+  let fullName = '';
+
+  // TODO: it’s necessary, but if something breaks, fix it here
+  if (name.substring(0, 3) === 'ank') {
+    const firstName = name.charAt(0).toUpperCase();
+    const lastName = name.substring(2);
+    fullName = `${lastName.charAt(0).toUpperCase() + name.slice(3)} ${firstName}.`;
+  } else {
+    const firstName = name.charAt(0).toUpperCase();
+    const lastName = name.substring(1);
+    fullName = `${lastName.charAt(0).toUpperCase() + name.slice(2)} ${firstName}.`;
+  }
   return (
     <div className="sidebar-infobox">
       <div className="sidebar-infobox__image">
         <IconPersonalProfile />
       </div>
-      <div className="sidebar-infobox__col">
-        <div className="sidebar-infobox__name">{name}</div>
+      <div className="sidebar-infobox__col" data-cy="sidebar-infobox">
+        <div className="sidebar-infobox__name">{fullName}</div>
       </div>
     </div>
   );
