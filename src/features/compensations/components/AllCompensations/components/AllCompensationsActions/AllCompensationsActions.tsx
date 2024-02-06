f7d9ca9dcd48e4017222e06d3233120292ba552c
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-02-06 09:22:46 +0500

    chore: remove search bar

--- src/features/compensations/components/AllCompensations/components/AllCompensationsActions/AllCompensationsActions.tsx
+++ src/features/compensations/components/AllCompensations/components/AllCompensationsActions/AllCompensationsActions.tsx
@@ -1,7 +1,6 @@
 import { AllCompensationsFilter } from './components/AllCompensationsFilter/AllCompensationsFilter';
 import { DatePickerAllCompensations } from './components/DateAllCompensations/DatePickerAllCompensations';
 
-// TODO add SearchBarAll
 export function AllCompensationsActions() {
   return (
     <div className="all-compensations-actions" data-cy="all-compensations-actions">
