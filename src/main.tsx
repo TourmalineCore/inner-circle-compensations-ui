e12fab012f2c58088b96db999512afffca8a57c6
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-02-11 17:14:12 +0500

    chore: remove unnecessary eslint disable

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import 'react-datepicker/dist/react-datepicker.css'
 
 import './styles/index.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
 
 import { ThemeProvider } from './theme/themeContext'
