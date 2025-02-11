7493b7336e04ee4721d140c5cdb3db7944db135c
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-02-11 17:06:58 +0500

    chore: delete comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import 'react-datepicker/dist/react-datepicker.css'
 
 import './styles/index.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import reportWebVitals from './reportWebVitals'
 
 import App from './App'
 
@@ -27,8 +27,3 @@ async function initApp() {
 }
 
 initApp()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
