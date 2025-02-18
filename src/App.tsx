4d2b814b9f8489bc791dfe84cad3d531c2238ae9
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-02-18 10:36:43 +0500

    chore: change config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { useMemo } from 'react'
+import { lazy, Suspense, useMemo } from 'react'
 import { withPrivateRoute } from './common/withPrivateRoute'
 import Template from './template/Template'
 import { AccessBasedOnPemissionsState } from './routes/state/AccessBasedOnPemissionsState'
 import { AccessBasedOnPemissionsStateContext } from './routes/state/AccessBasedOnPemissionsStateContext'
 
-import Sidebar from 'inner_circle_layout_ui/layout'
+// import Sidebar from 'inner_circle_layout_ui/layout'
+const Sidebar = lazy(
+  async () => import(`inner_circle_layout_ui/layout`),
+)
 
 const WithPrivateRoute = withPrivateRoute(Template)
 
@@ -30,9 +33,14 @@ export default function App() {
             element={<WithPrivateRoute />}
           />
         </Routes>
-        <Sidebar
+        {/* <Sidebar
           routesState={routesState}
-        />
+        /> */}
+        <Suspense fallback="loading...">
+          <Sidebar
+            routesState={routesState}
+          />
+        </Suspense>
       </BrowserRouter>
     </AccessBasedOnPemissionsStateContext.Provider>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/quotes */
 import { defineConfig } from 'vite'
+import { federation } from '@module-federation/vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import { federation } from '@module-federation/vite'
 
 const LOCAL_ENV_PORT = 40100
 const COMPENSATIONS_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 4003
@@ -13,37 +14,32 @@ export default defineConfig({
     port: COMPENSATIONS_PORT,
   },
   base: `/compensations`,
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
   plugins: [
     react(),
     svgr(),
     federation({
-      name: `inner_circle_compensations_ui`, // Unique name for the application
-      manifest: true,
+      name: "inner_circle_compensations_ui", // Unique name for the application
+      // manifest: true,
       remotes: {
         inner_circle_layout_ui: {
-          type: `module`,
-          name: `inner_circle_layout_ui`, // The unique name of the remote application that will be used for identification
+          type: "module",
+          name: "inner_circle_layout_ui", // The unique name of the remote application that will be used for identification
           entry: `http://localhost:${LAYOUT_PORT}/layout/remoteEntry.js`, // The URL where the manifest file for the remote application can be found
+          entryGlobalName: "inner_circle_layout_ui",
+          shareScope: "default",
         },
       },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: `^18.2.0`,
-        },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: `^18.2.0`,
-        },
-        'react-router-dom': {
-          singleton: true,
-          requiredVersion: `^6.2.2`,
-        },
-        'react/jsx-runtime': {
-          singleton: true,
-          requiredVersion: `^18.2.0`,
-        },
-      },
+      filename: "remoteEntry.js",
+      shared: [
+        "react",
+      ],
 
       /* singleton: true: This setting ensures that only a single instance of the specified module 
       (in this case, react) is loaded in the application. 
@@ -58,23 +54,7 @@ export default defineConfig({
   },
   build: {
     target: `chrome89`, // Setting the target browser version for the build
-    minify: false,
-    cssCodeSplit: false,
-    rollupOptions: {
-      external: [
-        /^__mf__virtual\/.*/,
-        `react`,
-        `react-dom`,
-        `react-router-dom`,
-        `react/jsx-runtime`,
-        `/compensations/env-config.js`,
-        `inner_circle_layout_ui/layout`,
-        `/node_modules`,
-      ],
-      output: {
-        format: `es`,
-        sanitizeFileName: (file) => file,
-      },
-    },
+    // minify: false,
+    // cssCodeSplit: false,
   },
 })
