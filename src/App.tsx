3a29a9a0c884b5063c3c4dcc6a71286387693d51
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-02-27 11:21:36 +0500

    chore: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { lazy, Suspense, useMemo } from 'react'
 import { AccessBasedOnPemissionsState } from './routes/state/AccessBasedOnPemissionsState'
 import { AccessBasedOnPemissionsStateContext } from './routes/state/AccessBasedOnPemissionsStateContext'
+import { AllCompensationsPage } from './pages/compensations/AllCompensationsPage'
+import { CompensationsPersonalPage } from './pages/compensations/CompensationsPersonalPage'
+import { CompensationsPage } from './pages/compensations/CompensationsPage'
 
 // import Sidebar from 'inner_circle_layout_ui/layout'
 const Sidebar = lazy(
@@ -23,6 +26,20 @@ export default function App() {
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}>
+        <Routes>
+          <Route
+            path="/compensations/my"
+            element={<CompensationsPersonalPage />}
+          />
+          <Route
+            path="/compensations/all"
+            element={<AllCompensationsPage />}
+          />
+          <Route
+            path="/compensations/"
+            element={<CompensationsPage />}
+          />
+        </Routes>
         {/* <Sidebar
           routesState={routesState}
         /> */}
