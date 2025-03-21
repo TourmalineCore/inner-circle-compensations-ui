546e7d08429e27052ff522930184fe21b7527b39
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-03-21 16:41:42 +0500

    chore: fix bug with auth redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useContext, useEffect } from 'react'
 import { getPageRoutes } from './routes/pageRoutes'
+import { authService } from './common/authService'
 
 // import Layout from remote app
 const Layout = lazy(
@@ -9,6 +10,20 @@ const Layout = lazy(
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
+
+  // @ts-ignore
+  const [
+    token,
+  ] = useContext(authService.AuthContext)
+
+  useEffect(() => {
+    if (!token) {
+      window.location.href = `/auth`
+    }
+  }, [
+    token,
+  ])
+
   return (
     <BrowserRouter future={{
       v7_startTransition: true,
