aa25910e8108c6b5b63a06782f6bb836fec86f1b
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-03-26 14:33:02 +0500

    fix: resolve missing env-config file at cypress component tests open

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,5 +1,13 @@
 import './commands'
-import '../env-config'
+
+window.__ENV__ = {
+  VITE_API_ROOT: `http://test.com/api`,
+  VITE_API_ROOT_AUTH: `http://test.com/auth-api`,
+  LINK_TO_ACCOUNT_SERVICE: `/`,
+  LINK_TO_COMPENSATIONS_SERVICE: `/`,
+  VITE_BASE_URL: `http://localhost:4455`,
+  LINK_TO_SALARY_SERVICE: `/`,
+}
 
 /// <reference types="cypress" />
 
