258f37267997580156a03b3642c0d0335438c364
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-04-16 14:48:15 +0500

    chore: remove unnecessary envs from component tests

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -3,11 +3,7 @@ import './commands'
 // @ts-ignore
 window.__ENV__ = {
   API_ROOT: `http://test.com/api`,
-  VITE_API_ROOT_AUTH: `http://test.com/auth-api`,
-  LINK_TO_ACCOUNT_SERVICE: `/`,
   LINK_TO_COMPENSATIONS_SERVICE: `/`,
-  VITE_BASE_URL: `http://localhost:4455`,
-  LINK_TO_SALARY_SERVICE: `/`,
 }
 
 /// <reference types="cypress" />
